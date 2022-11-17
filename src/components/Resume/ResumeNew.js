db5354d776d9841970e476bb05356e41f8c2a2b4
Prasad Adhav <prasadadhav22@gmail.com>
2022-11-17 20:08:06 +0530

    added pdf

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -3,13 +3,14 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 // import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
-import pdf from "../../Assets/../Assets/Mr. Prasad Resume.pdf";
+import pdf from "../../Assets/../Assets/Prasad_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink = "/src/Assets/Mr. Prasad Resume.pdf";
+const resumeLink =
+  "https://github.com/imdpc/Prasad-Adhav-Portfolio/blob/master/src/Assets/Mr.PrasadResume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
